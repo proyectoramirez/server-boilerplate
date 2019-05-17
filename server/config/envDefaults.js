df0d0bc6c2a8b4c092214f1544809547b5e3f60f
Ramirez Fuentes <jose56@usf.edu>
2019-05-17 16:34:46 -0400

    Changed config structure

--- server/config/envDefaults.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: "localhost",
-    port: "3000",
-    
-    db_server: "127.0.0.1:27017",
-    db_name: "Panus"
-}
--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,19 @@
-const argv = require("../utils/argv");
-const envDefaults = require("./envDefaults");
-const constants = require("./constants");
+const argv = require("@/utils/argv");
+const { envName } = require("@/utils/env");
+const requireOrDefault = require("@/utils/requireOrDefault");
 
-for (field in envDefaults) {
-    envDefaults[field] = argv[field] || process.env[field.toUpperCase()] || envDefaults[field];
+const base = require("./config.json");
+const overrides = requireOrDefault(`./config.${envName}.json`);
+
+const final = { ...base, ...overrides };
+
+for (field in final) {
+    if (typeof final[field] === 'string' || myVar instanceof String) {
+        final[field] = argv[field] || process.env[field.toUpperCase()] || final[field];
+    }
 }
 
-module.exports = {...envDefaults, ...constants};
+module.exports = final;
 
 //TO START MONGO
 //"D:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="d:\mongo\db"
