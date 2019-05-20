ceab5344a8b9da04fa671cca708833388d342c41
Jose Ramirez <proyectoramirez07@gmail.com>
2019-05-20 00:53:34 -0400

    Re-organized server structure

--- server/index.js
+++ server/index.js
@@ -2,25 +2,25 @@
 require('module-alias/register');
 
 const express = require("express");
-const logger = require("./utils/logger");
 const config = require("./config");
 const db = require("./database");
 const middleware = require("./middleware");
 const routes = require("./routes");
+const logger = require("./utils/logger");
 
 const app = express();
 
 app.use(middleware);
 app.use(routes);
 
-db.createConnection().then(startApp);
+db.createConnection().finally(startServer);
 
-function startApp() {
+function startServer() {
     const host = config.host;
     const port = config.port;
 
     // Start your app.
-    app.listen(port, host, async err => {
+    app.listen(port, host, err => {
         if (err) {
             return logger.error(err);
         }
