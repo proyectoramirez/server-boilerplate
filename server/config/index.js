748ea7fd64e043a1d3185295c0def94264a846de
Jose Ramirez <proyectoramirez07@gmail.com>
2019-05-22 00:11:17 -0400

    Solved the requireOrDefault bug

--- server/config/index.js
+++ server/config/index.js
@@ -1,20 +1,23 @@
+const path = require("path");
+const deepFreeze = require('deep-freeze');
 const argv = require("@/utils/argv");
 const { envName } = require("@/utils/env");
 const requireOrDefault = require("@/utils/requireOrDefault");
-const deepFreeze = require('deep-freeze');
 
 const base = require("./config.js");
-const overrides = requireOrDefault(`./config.${envName}.js`);
+const overrides = requireOrDefault(path.resolve(__dirname, `./config.${envName}.js`));
 
 const final = { ...base, ...overrides };
 
 for (field in final) {
-    if (typeof final[field] === 'string' || myVar instanceof String) {
+    if (typeof final[field] === 'string' || final[field] instanceof String) {
         final[field] = argv[field] || process.env[field.toUpperCase()] || final[field];
     }
 }
 
-module.exports = deepFreeze(final);
+const frozen = deepFreeze(final);
+
+module.exports = frozen;
 
 //TO START MONGO
 //"D:\Program Files\MongoDB\Server\4.0\bin\mongod.exe" --dbpath="d:\mongo\db"
